dabe66fe471d93c4e88e3401f6602766eebc01ee
Starwars432 <manifestillusions@gmail.com>
2025-06-25 21:22:46 -0400

    Fix missing grapesjs-global import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './lib/grapesjs-global'; // Import early to make grapesjs globally available
 import App from './App';
 import './index.css';
 
